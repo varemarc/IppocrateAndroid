a0c0144764542d329423caf1bcd1b8b4683b99ba
varemarc <varemarc@gmail.com>
2014-03-17 18:43:14 +0100

    Quasi terminata fase di login. In caso di login fallito viene visualizzato un messaggio in rosso. Da implementare la fase di successo. Aggiunto xml contenente gli indirizzi per la connessione ad Ippocrate in locale (ognuno dovra' modificarlo con i propri valori). Aggiunto xml di layout per il messaggio di errore (Toast). Aggiunta libreria per trattare i JSON.

--- src/com/example/ippocrate/MainActivity.java
+++ src/com/example/ippocrate/MainActivity.java
@@ -1,43 +1,122 @@
 package com.example.ippocrate;
 
+import org.json.JSONObject;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		StrictMode.enableDefaults();
+
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+				.permitAll().build();
+
+		StrictMode.setThreadPolicy(policy);
+
 		super.onCreate(savedInstanceState);
 		getActionBar().hide();
 		setContentView(R.layout.activity_main);
 
+		Button accedi = (Button) findViewById(R.id.accedi);
+		accedi.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+
+				EditText et1 = (EditText) findViewById(R.id.username);
+				EditText et2 = (EditText) findViewById(R.id.pincode);
+				EditText et3 = (EditText) findViewById(R.id.password);
+
+				String username = et1.getText().toString();
+				String pincode = et2.getText().toString();
+				String password = et3.getText().toString();
+
+				if (login(username, pincode, password)) {
+					// TODO: lanciare una nuova Activity e passare il parametro idM
+				} else {
+					LayoutInflater inflater = getLayoutInflater();
+					View layout = inflater.inflate(R.layout.error_toast,
+							(ViewGroup) findViewById(R.id.error_toast_layout));
+
+					TextView text = (TextView) layout.findViewById(R.id.error_toast_text);
+					text.setText("Login fallito!");
+					text.setTypeface(null, Typeface.BOLD);
+
+					Toast toast = new Toast(getApplicationContext());
+					toast.setDuration(Toast.LENGTH_SHORT);
+					toast.setView(layout);
+					toast.show();
+				}
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	/** Metodo invocato al click del pulsante di login */
+	public boolean login(String username, String pincode, String password) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+		Log.i("login", "fase iniziale");
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		SoapObject request = new SoapObject(getString(R.string.NAMESPACE),
+				"effettuaLogin");
+		request.addProperty("username", username);
+		request.addProperty("pincode", pincode);
+		request.addProperty("password", password);
+
+		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+				SoapEnvelope.VER11);
+		envelope.setAddAdornments(false);
+		envelope.implicitTypes = true;
+		envelope.setOutputSoapObject(request);
+
+		HttpTransportSE androidHttpTransport = new HttpTransportSE(
+				getString(R.string.URL));
+
+		androidHttpTransport.debug = true;
+		try {
+			androidHttpTransport.call("effettuaLogin", envelope);
+
+			Log.i("login", "inviata richiesta");
+
+			SoapPrimitive response = (SoapPrimitive) envelope.getResponse();
+
+			Log.i("login", "ricevuta risposta");
+
+			String responseData = response.toString();
+
+			JSONObject obj = new JSONObject(responseData);
+			String ris = (String) obj.get("loginOK");
+
+			Log.i("loginOK", ris);
+
+			if (ris.equals("-1") == false) {
+				Log.i("login", "confermato");
+				return true;
+			} else {
+				Log.i("login", "rifiutato");
+				return false;
+			}
+
+		} catch (Exception e) {
+			Log.e("WS Error->", e.toString());
+			return false;
 		}
-		return super.onOptionsItemSelected(item);
 	}
 }
